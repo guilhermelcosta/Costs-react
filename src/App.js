6ae3e9485baf7d44b72f0becd20daa8596bc7416
Guilherme Lage da costa <guilhermeldcosta@gmail.com>
2023-07-06 18:54:49 -0300

    tada: upload inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Empresa from './components/pages/Empresa';
+import Contato from './components/pages/Contato';
+
+import Container from './components/layout/Container';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
+import Projetos from './components/pages/Projetos';
+import NovoProjeto from './components/pages/NovoProjeto';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <NavBar/>
+
+            <Container customClass="min-height">
+                <Routes>
+                    <Route exact path="/" element={<Home/>}></Route>
+                    <Route path="/projetos" element={<Projetos/>}></Route>
+                    <Route path="/empresa" element={<Empresa/>}></Route>
+                    <Route path="/contato" element={<Contato/>}></Route>
+                    <Route path="/novoprojeto" element={<NovoProjeto/>}></Route>
+                </Routes>
+            </Container>
+
+            <Footer/>
+        </Router>
+    );
 }
 
 export default App;
