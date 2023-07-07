5e945f1b1be4f4f9b5712afd7533a909c72e7c66
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-07-07 14:53:31 -0300

    tada: new upload

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
     return (
         <nav className={styles.navbar}>
             <Container>
-                <Link to={'/'}> <img src={logo} alt={'Costs'}/> </Link>
+                <Link to={'/'}><img src={logo} alt={'Costs'}/></Link>
                 <ul className={styles.list}>
                     <li className={styles.item}><Link to={'/'}>Home</Link></li>
                     <li className={styles.item}><Link to={'/empresa'}>Empresa</Link></li>
